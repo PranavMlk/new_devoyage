d0e2c0585b7d3df6d8495f392c44a797c8ea5b87
Pranav <pranavmlk20@gmail.com>
2022-10-13 12:02:22 +0530

    mys

--- lib/sign up/sign_up.dart
+++ lib/sign up/sign_up.dart
@@ -32,7 +32,7 @@ class _SignUpState extends State<SignUp> {
                   children:[ Container(
                     margin: EdgeInsets.only(top: 7,left: 20),
                     alignment: Alignment.centerLeft,
-                    child: Text('Sign up',
+                    child: Text('Sign uuuup',
                       style: TextStyle(
                           color: primaryColor,
                           fontSize: 35,
